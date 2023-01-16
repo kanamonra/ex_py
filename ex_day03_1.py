25078ad15621b94dd4e98fc2ed364fb0e8ac627d
unknown <kanamonra11>
2023-01-16 01:15:09 -0800

    ex_6.2

--- ex_day03_1.py
+++ ex_day03_1.py
@@ -1,7 +1,13 @@
-# 5.8 f{} 5.6
-Boat = 'Duck'
-Hourse = 'Gourd'
-Train = 'Spitz'
-print(f'{Boat}y Mc{Boat}face')
-print(f'{Hourse}y Mc{Hourse}face')
-print(f'{Train}y Mc{Train}face')
+# 6.2
+guess_me = 7
+number = 1
+while number < guess_me:
+    number = number + 1
+    if number == guess_me:
+        print('Found it!')
+        break
+    if number > guess_me:
+        print('too_low')
+
+    else:
+        print('oops')
