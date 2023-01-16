166dd6606e2a2f97a9004754ad577952125b41a2
unknown <kanamonra11>
2023-01-16 00:03:31 -0800

    ex_5.4

--- ex_day03_1.py
+++ ex_day03_1.py
@@ -1,24 +1,17 @@
-# input 2 numbers
-# start_end = input('Start and end number: ').split()
-# print(start_end)
-# print(int(start_end[0], int(start_end[1])))
-# start = int(input("start number: "))
-# end = int(input("end number: "))
-# print(start, end)
-# for k in range(start, end + 1):
-#    print(k, end='  ')
-start = int(input("Please enter the start number: "))
-end = int(input("Please enter the end number: "))
-if end < start:
-    start, end = end, start  # ингэж байрыг нь сольж өгнө
-for i in range(start, end + 1):
-    # is_prime = True
+# 5.4 "새 스타일로 매일 쓰기"
+# 'Dear {1} {0},'.format(salutation, name)
+# 'Hello, {}'.format(name)
 
-    if i <= 1:
-        continue
-    for k in range(2, i):
-        if i % k == 0:
-            # is_prime = False
-            break
-    else:
-        print(i, end='  ')
+letter_1 = '    Dear {0} {1}'.format('salutation', 'name')
+letter_2 = '    Thank you for your letter. We are sorry that our {0} {1} in your {2}.'.format('product', 'verbed', 'room')
+letter_3 = '    Please note that it should never be used in {0}, especially near any {1}.'.format('room', 'animals')
+letter_4 = '''    Send us your receipt and {0} for shipping and handling. We will send you another {1} that, in our tests, in {2}% less likely to have {3}'''.format('amount', 'product', 'percent', 'verbed')
+letter_5 = '''        Thank you for you support.
+        Sincerely,
+        {0}
+        {1}'''.format('Spokesman', 'job_title')
+print(letter_1)
+print(letter_2)
+print(letter_3)
+print(letter_4)
+print(letter_5)
