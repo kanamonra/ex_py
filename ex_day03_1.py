f9d0fe3c6872f4df08d645cea8473b76baec42eb
unknown <kanamonra11>
2023-01-16 01:04:25 -0800

    ex_5.8

--- ex_day03_1.py
+++ ex_day03_1.py
@@ -1,4 +1,7 @@
-# 5.7 format() 5.6
-print('{}y Mc{}face'.format('Duck','Duck'))
-print('{}y Mc{}face'.format('Gourd','Gourd'))
-print('{}y Mc{}face'.format('Spitz','Spitz'))
+# 5.8 f{} 5.6
+Boat = 'Duck'
+Hourse = 'Gourd'
+Train = 'Spitz'
+print(f'{Boat}y Mc{Boat}face')
+print(f'{Hourse}y Mc{Hourse}face')
+print(f'{Train}y Mc{Train}face')
