5f3d50a83ea8845ba4bf62b67fcb4fc6ab98afad
unknown <kanamonra11>
2023-01-17 14:11:57 +0900

    day04_dic

---
+++ w02_tue.py
@@ -0,0 +1,8 @@
+# exercise 7.4
+things = ['mozarella', 'cinderella', 'salmonella']
+# exercise 7.5 caps
+print(things[-2].title())
+# exercise 7.6
+things[0] = things[0].upper()
+# exercise 7.7
+print(f'Delete the {things.pop()} from things, to get Nobel Prize')
