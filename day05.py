e8ddcb39cdb49080f04e86e0cc463e25eec7444b
unknown <kanamonra11>
2023-01-18 18:53:32 +0900

    day05_8.14

--- day05.py
+++ day05.py
@@ -1,5 +1,16 @@
-# 9.1 good function
-def good():
-    return ['Harry', 'Ron', 'Hermione']
-
-print(good())
+# 8.10
+square = {number: number**2 for number in range(10)}
+print(square)
+# 8.11
+set_square = {number for number in range(10) if number % 2 == 0}
+print(set_square)
+# 8.13 zip()
+a = ("The glass is half full", 'The glass is half empty', 'How did you get a glass?')
+b = ('optimist', 'persist', 'troll')
+zip_it = dict(zip(a, b))
+print(zip_it)
+# 8.14 movie dictionary
+titles = ['Creature of Habit', 'Crewel Fate']
+plots = ['A nun turns into a monster', 'A haunted yarn shop']
+movies = dict(zip(titles, plots))
+print(movies)
