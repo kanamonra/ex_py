8158a9e511a12a2a06f6103bd4abd5672b9a8f22
unknown <kanamonra11>
2023-01-18 17:20:11 +0900

    day05_8.9

--- day05.py
+++ day05.py
@@ -1,1 +1,18 @@
-# 8.6 life dictionary
+# 8.6 life dictionary deep copy exercise
+life = {
+    'animals': {
+        'cats': [
+            'Henry', 'Grumpy', 'Lucy'
+        ],
+        'octopi': {},
+        'emus': {}
+    },
+    'plants': {},
+    'other': {}
+}
+# 8.7 life dictionary 1st key
+# print(life.keys())
+# 8.8 life[animals] key 출력
+print(life['animals'].keys())
+# 8.9 life[animals, cats] all key 출력
+print(life['animals'])
