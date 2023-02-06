969f4bc2ab5464ef9f850887a20beabaa2ad9eb6
kanamonra <ceceg@inha.edu>
2023-02-06 21:38:38 +0900

    day16_Ex6.1

--- day15.py
+++ day15.py
@@ -1,47 +1,68 @@
 # 이중 연결 리스트 구현하기
-# 199p - 5장 응용예제 02
-# 난이도 2점
-
-class Node:
-    def __init__(self):
-        self.plink = None  # first
-        self.data = None
-        self.nlink = None  # at end
+# 228p - 6장 응용예제 01
+# 난이도 1점
+import random
 
 
-def print_node(start):
-    current = start
-    if current.nlink == None:
-        return
-    print('정방향 -->', end=' ')
-    print(current.data, end=" ")
-    # end link
-
-    while current.nlink != None:
-        current = current.nlink
-        print(current.data, end=' ')
-    print()
-    print('역방향 -->', end=' ')
-    print(current.data, end=" ")
-    # first link
-    while current.plink != None:
-        current = current.plink
-        print(current.data, end=' ')
+def isStackFull():
+    global stack, SIZE, top
+    if top >= SIZE - 1:
+        return True
+    else:
+        return False
 
 
-head, current, pre = None, None, None
-data_array = ['다현', '정연', '쯔위', '사나', '지효']
+def isStackEmpty():
+    global stack, top
+    if top == -1:
+        return True
+    else:
+        return False
+
+
+def push(data):
+    global stack, top
+    if isStackFull():
+        print('Stack is full')
+    top += 1
+    stack[top] = data
+
+
+def pop():
+    global stack, top, SIZE
+    if isStackEmpty():
+        return None
+    data = stack[top]
+    stack[top] = None
+    top -= 1
+    return data
+
+
+def peek():
+    global stack, SIZE, top
+    if isStackEmpty():
+        return None
+    return stack[top]
+
+
+SIZE = 7
+stack = [None for _ in range(SIZE)]
+top = -1
 
 if __name__ == "__main__":
-    node = Node()
-    node.data = data_array[0]
-    head = node
+    stone_array = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+    random.shuffle(stone_array)
 
-    for data in data_array[1:]:
-        pre = node
-        node = Node()
-        node.data = data
-        pre.nlink = node
-        node.plink = pre
+    print("Way to candy house: ", end=" ")
+    for stone in stone_array:
+        push(stone)
+        print(stone, "-->", end=" ")
+    print("Our home")
 
-    print_node(head)
+    print("Way to home: ", end=" ")
+    while True:
+        stone = pop()
+        if stone == None:
+            break
+        print(stone, "-->", end= " ")
+    print("Candy house")
