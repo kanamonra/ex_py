de8b77639e6839f9aae368a0fe01d185a0fb4aa3
kanamonra <ceceg@inha.edu>
2023-02-20 15:23:59 +0900

    day26 binarySearch

--- day26Search.py
+++ day26Search.py
@@ -1,20 +1,30 @@
-# Searching algorithm
-def search(array, array_length, input_num):
-    for i in range(0, array_length):
-        if array[i] == input_num:
-            return i
-    return -1
+# Interval search -> Binary search
+# more fast for finding non exist data
+
+def binary_search(arr, find_data):
+    pos = -1
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if find_data == arr[mid]:
+            return mid
+        elif find_data > arr[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return pos
 
 
-# Driver Code
-if __name__ == "__main__":
-    arr = [2, 3, 4, 10, 40, 50, 66, 76, 95, 230]
-    x = 66
-    N = len(arr)
+data_array = [2, 5, 6, 12, 34, 45, 65, 91, 99, 120, 142, 162, 234]
 
-    # Search function call
-    result = search(arr, N, x)
-    if result == -1:
-        print("Element is not present in array")
-    else:
-        print("Element is present at index", result)
+findData = 99
+
+print('Array -->', data_array)
+position = binary_search(data_array, findData)
+if position == -1:
+    print("Not found...", findData)
+else:
+    print(findData, 'is', position, "th place in data")
